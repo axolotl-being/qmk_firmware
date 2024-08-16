6fc97f915dbf596a3b78e71a01bc1dc3ce495b16
axolotl-being <nora.and.the.moon@gmail.com>
2024-08-15 23:55:29 -0500

    change number format on lower layer to numpad-like layout
    add georgia's keymap

--- keyboards/epomaker/tide75/keymaps/personal_v1/keymap.c
+++ keyboards/epomaker/tide75/keymaps/personal_v1/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,   KC_HOME,  
         KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  BCKSPC,  KC_END,    
         COPA,     KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_ENT,   KC_PGUP,          
-        KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     OSL(2),   KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT),      KC_UP,    KC_PGDN,
+        KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,OSL(_LOWER),   KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT),      KC_UP,    KC_PGDN,
         KC_LCTL,  KC_LALT,  OSM(MOD_LSFT),  KC_SPC,             KC_RALT,  KC_SPC,   KC_SPC,             LOWER,    RAISE,    KC_RGUI,  KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
 	
@@ -68,10 +68,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_LOWER] = LAYOUT( 
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,  KC_MS_BTN1,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   
-        _______, KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  
-        _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,              _______, 
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
-        _______,  _______,  _______,  _______,             _______,  _______,   _______,             _______,  _______,  _______,  _______,  _______,  _______
+        _______,  _______,     KC_7,     KC_8,     KC_9,  _______,  _______,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,  _______,  
+        _______,  _______,     KC_4,     KC_5,     KC_6,  KC_MINS,   KC_EQL,    KC_F4,    KC_F5,    KC_F6,  _______,  _______,  _______,              _______, 
+        _______,     KC_1,     KC_2,     KC_3,  _______,  _______,  _______,    KC_F1,    KC_F2,    KC_F3,  _______,  _______,            KC_MS_U,  KC_MS_BTN2,
+        _______,  _______,  _______,     KC_0,             _______,  _______,   _______,             _______,  _______,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R
     ),
 	[_RAISE] = LAYOUT( 
         EE_CLR,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,  KC_MS_BTN1,
